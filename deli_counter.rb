c405e1a853381ea8387f88adf92fe6c4f5c96f5c
Samara Grant <samara.amanda01@gmail.com>
2020-07-13 18:40:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,20 +13,16 @@ end
 
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length == 0
     katz_deli << (name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  else katz_deli.length >= 1
-    katz_deli << (name)
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  end
 end
 
+
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
-  else katz_deli.length >= 1
+  else
     puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift()
+    katz_deli.shift
   end
 end
